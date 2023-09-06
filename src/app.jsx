b897c86880ea565bafdf92736645a05bf205756e
dharmikpatel21 <dharmikjpatel21@gmail.com>
2023-09-06 11:59:37 +0530

    husky discarded and added performance measures in webpack common

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const bgColor = "red"
-const color = "green"
+const bgColor = 'red';
+const color = 'green';
 const App = () => {
     return (
         <>
-            <h1 style={{
-                backgroundColor: bgColor,
-                color 
-                }}>
+            <h1
+                style={{
+                    backgroundColor: bgColor,
+                    color,
+                }}
+            >
                 Dharmik Patel
-                </h1>
+            </h1>
             <p>Lorem ipsum dolor sit amet.</p>
         </>
     );
